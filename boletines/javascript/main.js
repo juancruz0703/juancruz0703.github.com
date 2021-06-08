070323af15357663ab4af3ff5edc2a773376db46
juancruz0703 <juancruzurquizagonzales@gmail.com>
2021-06-08 11:01:08 -0300

    javascript

---
+++ boletines/javascript/main.js
@@ -0,0 +1,20 @@
+let links = document.querySelectorAll(".close");
+
+links.forEach(function(link){
+    //Agregar un evento click a cada uno de ellos - case sensitive
+    link.addEventListener("click",function(ev){
+        ev.preventDefault();
+        let content = document.querySelector(".content");
+        //Quitar animaciones
+        content.classList.remove("fadeInDown");
+        content.classList.remove("animated");
+        //Agregar animaciones de salida
+        content.classList.add("fadeOutUp");
+        content.classList.add("animated");
+        setTimeout(function () {
+            location.href = "/";
+        },600)
+
+        return false;
+    })
+});
